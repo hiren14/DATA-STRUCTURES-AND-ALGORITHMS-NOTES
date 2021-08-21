3591601861b1320479c76d4d77c12f934383e073
HIREN lalani <72781029+hiren14@users.noreply.github.com>
2021-08-21 11:10:39 +0530

    Update Bubble sort.c
    Edited

--- Sorting/Bubble sort.c
+++ Sorting/Bubble sort.c
@@ -9,65 +9,43 @@ Complexity - O(n^2)
 
 void main()
 {
-	int array[MAXSIZE];
-	int i, j, num, temp;
-FILE *file1;  
-   FILE *file2; 
+int *ptr=NULL;
+int i, j, num, temp;
 
-	printf("Enter the value of num \n");
-	scanf("%d", &num);
+printf("Enter the value of num \n");
+scanf("%d", &num);
+ptr=(int*)malloc(num*sizeof(int));
 
-	printf("Before sorting the elements are \n");
-	for (i = 0; i < num; i++)
-	{
-		array[i]=rand()%100;/*initialize the random 
-function under 100 num*/
-	}
+printf("Before sorting the elements are \n");
 for (i = 0; i < num; i++)
-	{
-printf(" element %d is : %d\n",i,array[i]);		
-	}
-/*file handling */
-file1=fopen("UNSORTED.txt","w");   /* use of fopen ehich open 
-files and name is unsorted.txt and w =write if r is read */
-  fprintf(file1,"\n Unsorted element number\n"); 
+{
+ptr[i]=rand()%100;
+}
 for (i = 0; i < num; i++)
-	{
-fprintf(file1,"%d\n",*(prt+i)); 
-//printf(" element %d is : %d\n",i,ptr[i]);		
-	}
+{
+printf(" element %d is : %d\n",i,ptr[i]);
+}
 
+   /*   Bubble sorting begins */
+for (i = 0; i < num; i++)
+{
+for (j = 0; j < (num - i - 1); j++)
+{
+if (ptr[j] > ptr[j + 1])
+{
+temp = ptr[j];
+ptr[j] = ptr[j + 1];
+ptr[j + 1] = temp;
+}
+}
+}
 
-   /*   Bubble sorting begins */
-	for (i = 0; i < num; i++)
-	{
-		for (j = 0; j < (num - i - 1); j++)
-		{
-			if (array[j] > array[j + 1])
-			{
-				temp = array[j];
-				array[j] = array[j + 1];
-				array[j + 1] = temp;
-			}
-		}
-	}
-	
-	printf("Sorted array is...\n");
-	for (i = 0; i < num; i++)
-	{
-		printf("element %d is :%d\n",i, array[i]);
-	}
+printf("Sorted array is...\n");
+for (i = 0; i < num; i++)
+{
+printf("element %d is :%d\n",i, ptr[i]);
+}
 
-file2=fopen("SORTED.txt","w");   //opens the file 
-  fprintf(file2,"\n sorted element number\n");     //fprint the elements   
-   for ( i = 1; i < a+1; i++) 
-    {        
- fprintf(file2,"%d\n",*(prt+i)); /*fprint is used to 
-Printing the file and output in file */ 
-    }         
- fclose(file1);   //close the file 
-  fclose(file2);  //close the file 
-	
 }
 
 /*
